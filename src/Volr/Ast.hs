14c086b958407a82cd541292173733ae602a30e6
Jens Egholm <jensegholm@protonmail.com>
2018-03-29 15:58:02 +0200

    Refitted model to new concept of stimuli and strategies

--- src/Volr/Ast.hs
+++ src/Volr/Ast.hs
@@ -1,67 +1,36 @@
 module Volr.Ast
   ( Model(Model)
-  , SurfacePhenomena(SurfacePhenomena)
-  , Input(Input)
-  , Output(Output)
-  , As(As)
-  , Ef(Ef)
+  , Strategy(Strategy)
+  , SelectionCriterion(Accuracy, Random)
+  , Stimuli(Stimuli)
+  , Response(Response)
   , Features
   )
  where
 
-  -- | A model consisting of modules
+  -- | A model of the learning process
 data Model = Model
-  { input :: Input
-  , modules :: [SurfacePhenomena]
-  , output :: Output
+  { strategies :: [Strategy]
+  , response :: Response
   } deriving (Eq, Show)
 
-class WithFeatures a where
-  inputFeatures :: a -> Int
-  outputFeatures :: a -> Int
+data Strategy
+  = Strategy String [Stimuli] Int
+  deriving (Eq, Show)
 
-class WithStrategies a where
-  strategies :: a -> [As]
-  -- TODO: pickStrategy :: [As] -> As
+data SelectionCriterion
+  = Accuracy
+  | Random
+  deriving (Eq, Show)
 
-data SurfacePhenomena = SurfacePhenomena
-  { inputFeaturesPhenomena :: Int
-  , outputFeaturesPhenomena :: Int
-  , strategiesFromPhenomena :: [As]
-  } deriving (Eq, Show)
+data Stimuli
+  = Stimuli String String Features
+  | StrategyStimuli Strategy
+  deriving (Eq, Show)
 
-instance WithStrategies SurfacePhenomena where
-  strategies = strategiesFromPhenomena
-
-data Input = Input
-  { inputFeaturesFromInput :: Features
-  , outputFeaturesFromInput :: Features
-  } deriving (Eq, Show)
-
-instance WithFeatures Input where
-  inputFeatures = inputFeaturesFromInput
-  outputFeatures = outputFeaturesFromInput
-
-data Output = Output
-  { inputFeaturesFromOutput :: Features
-  , outputFeaturesfromOutput :: Features
-  } deriving (Eq, Show)
-
-instance WithFeatures Output where
-  inputFeatures = inputFeaturesFromOutput
-  outputFeatures = outputFeaturesfromOutput
-
--- | An algorithmic strategy containing a number of EFs
-newtype As = As
-  { efs :: Int
-  } deriving (Eq, Show)
-
--- | An elementary function (EF) consisting of a given number of neurons
---   and the probability threshold of connecting two neurons in the EF
-data Ef = Ef
-  { neurons :: Int
-  , connectivityProbability :: Float
-  }
+data Response
+  = Response String Features
+  deriving (Eq, Show)
 
 -- | A number of boolean features to express in the model
 type Features = Int
